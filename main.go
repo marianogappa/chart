dbacfdd3dd937fdfd3d6eb7dd8120827640ca5a5
Mariano Gappa <mariano@movio.co>
2016-12-26 13:02:53 +1300

    Separates building chart from rest of main()

--- main.go
+++ main.go
@@ -2,19 +2,43 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 	"text/template"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/skratchdot/open-golang/open"
 )
 
 func main() {
 	o := mustResolveOptions(os.Args[1:])
 	i := readInput(os.Stdin)
+
+	b, err := buildChart(i, o)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tmpfile, err := ioutil.TempFile("", "chartData")
+	if err != nil {
+		log.WithField("err", err).Fatalf("Could not create temporary file to store the chart.")
+	}
+
+	if err = baseTemplate.Execute(tmpfile, b.String()); err != nil {
+		log.WithField("err", err).Fatalf("Could not write chart to temporary file.")
+	}
+	if err = tmpfile.Close(); err != nil {
+		log.WithField("err", err).Fatalf("Could not close temporary file after saving chart to it.")
+	}
+
+	open.Run("file://" + tmpfile.Name())
+}
+
+func buildChart(i []string, o options) (bytes.Buffer, error) {
 	fss, sss, tss, minFSS, maxFSS, o := preprocess(i, o)
 
+	var b bytes.Buffer
 	var err error
 	var templ *template.Template
 	var templData interface{}
@@ -30,24 +54,11 @@ func main() {
 		templData, templ, err = setupScatter(fss, sss, tss, minFSS, maxFSS, o.title, o.scaleType, o.xLabel, o.yLabel)
 	}
 	if err != nil {
-		log.Fatalf("Could not construct chart because [%v]", err)
+		return b, fmt.Errorf("Could not construct chart because [%v]", err)
 	}
-	var b bytes.Buffer
 	if err := templ.Execute(&b, templData); err != nil {
-		log.Fatalf("Could not prepare ChartJS js code for chart: [%v]", err)
+		return b, fmt.Errorf("Could not prepare ChartJS js code for chart: [%v]", err)
 	}
 
-	tmpfile, err := ioutil.TempFile("", "chartData")
-	if err != nil {
-		log.Fatalf("Could not create temporary file to store the chart: [%v]", err)
-	}
-
-	if err = baseTemplate.Execute(tmpfile, b.String()); err != nil {
-		log.Fatalf("Could not write chart to temporary file: [%v]", err)
-	}
-	if err = tmpfile.Close(); err != nil {
-		log.Fatalf("Could not close temporary file after saving chart to it: [%v]", err)
-	}
-
-	open.Run("file://" + tmpfile.Name())
+	return b, nil
 }
