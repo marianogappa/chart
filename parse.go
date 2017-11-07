5b99d4d35c00e75b0e48ce8bd09f533d6b5309a9
Sam Zaydel <szaydel@gmail.com>
2017-11-07 13:01:57 -0800

    Parsing is not successful when data appears tabular, but separator is actually multiple space characters

--- parse.go
+++ parse.go
@@ -46,10 +46,21 @@ func maxLineFormat(lfs map[string]int) string {
 }
 
 func parseLineFormat(s string, sep rune, df string) string {
+	var sp []string
 	s = string(regexp.MustCompile(string(sep)+"{2,}").ReplaceAll([]byte(s), []byte(string(sep))))
-	ss := strings.Split(strings.TrimSpace(s), string(sep))
+	sp = strings.Split(strings.TrimSpace(s), string(sep))
+	// In certain cases data may appear to be tabbed, but
+	// instead there is an irregular number of spaces used
+	// as separator, rather than a tabstop. In such a case
+	// parsing above may result in a slice with a single
+	// element, not correctly parsed. In this case we want
+	// to attempt split using strings.Fields as opposed to
+	// strings.Split.
+	if len(sp) == 1 && strings.Count(sp[0], " ") > 0 {
+		sp = strings.Fields(strings.TrimSpace(s))
+	}
 	lf := ""
-	for _, sc := range ss {
+	for _, sc := range sp {
 		if _, err := strconv.ParseFloat(sc, 64); err == nil {
 			lf += "f"
 		} else if _, err := time.Parse(df, sc); err == nil && strings.TrimSpace(sc) != "" {
@@ -62,8 +73,14 @@ func parseLineFormat(s string, sep rune, df string) string {
 }
 
 func parseLine(l string, lf string, sep rune, df string) ([]float64, []string, []time.Time, error) {
+	var sp []string
 	l = string(regexp.MustCompile(string(sep)+"{2,}").ReplaceAll([]byte(l), []byte(string(sep))))
-	sp := strings.Split(strings.TrimSpace(l), string(sep))
+	sp = strings.Split(strings.TrimSpace(l), string(sep))
+	// See explanation in parseLineFormat for this seemingly
+	// unnecessary splitting code.
+	if len(sp) == 1 && strings.Count(sp[0], " ") > 0 {
+		sp = strings.Fields(strings.TrimSpace(l))
+	}
 
 	fs := []float64{}
 	ss := []string{}
@@ -74,6 +91,7 @@ func parseLine(l string, lf string, sep rune, df string) ([]float64, []string, [
 	}
 
 	for i, lfe := range lf {
+
 		s := strings.TrimSpace(sp[i])
 		switch lfe {
 		case 's':
