69684bc374f903d1c76dc12218f8a9967ce44df9
Mariano Gappa <mariano@movio.co>
2018-08-05 17:36:56 +1200

    Tests additional options.

--- options_test.go
+++ options_test.go
@@ -117,6 +117,138 @@ func TestResolveOptions(t *testing.T) {
 				lineFormat: format.LineFormat{Separator: '\t'},
 			},
 		},
+		{
+			args: []string{"ANSIC"},
+			expected: options{
+				dateFormat: "Mon Jan _2 15:04:05 2006",
+				separator:  '\t',
+				lineFormat: format.LineFormat{Separator: '\t', DateFormat: "Mon Jan _2 15:04:05 2006"},
+			},
+		},
+		{
+			args: []string{"UnixDate"},
+			expected: options{
+				dateFormat: "Mon Jan _2 15:04:05 MST 2006",
+				separator:  '\t',
+				lineFormat: format.LineFormat{Separator: '\t', DateFormat: "Mon Jan _2 15:04:05 MST 2006"},
+			},
+		},
+		{
+			args: []string{"RubyDate"},
+			expected: options{
+				dateFormat: "Mon Jan 02 15:04:05 -0700 2006",
+				separator:  '\t',
+				lineFormat: format.LineFormat{Separator: '\t', DateFormat: "Mon Jan 02 15:04:05 -0700 2006"},
+			},
+		},
+		{
+			args: []string{"RFC822"},
+			expected: options{
+				dateFormat: "02 Jan 06 15:04 MST",
+				separator:  '\t',
+				lineFormat: format.LineFormat{Separator: '\t', DateFormat: "02 Jan 06 15:04 MST"},
+			},
+		},
+		{
+			args: []string{"RFC822Z"},
+			expected: options{
+				dateFormat: "02 Jan 06 15:04 -0700",
+				separator:  '\t',
+				lineFormat: format.LineFormat{Separator: '\t', DateFormat: "02 Jan 06 15:04 -0700"},
+			},
+		},
+		{
+			args: []string{"RFC850"},
+			expected: options{
+				dateFormat: "Monday, 02-Jan-06 15:04:05 MST",
+				separator:  '\t',
+				lineFormat: format.LineFormat{Separator: '\t', DateFormat: "Monday, 02-Jan-06 15:04:05 MST"},
+			},
+		},
+		{
+			args: []string{"RFC1123"},
+			expected: options{
+				dateFormat: "Mon, 02 Jan 2006 15:04:05 MST",
+				separator:  '\t',
+				lineFormat: format.LineFormat{Separator: '\t', DateFormat: "Mon, 02 Jan 2006 15:04:05 MST"},
+			},
+		},
+		{
+			args: []string{"RFC1123Z"},
+			expected: options{
+				dateFormat: "Mon, 02 Jan 2006 15:04:05 -0700",
+				separator:  '\t',
+				lineFormat: format.LineFormat{Separator: '\t', DateFormat: "Mon, 02 Jan 2006 15:04:05 -0700"},
+			},
+		},
+		{
+			args: []string{"RFC3339"},
+			expected: options{
+				dateFormat: "2006-01-02T15:04:05Z07:00",
+				separator:  '\t',
+				lineFormat: format.LineFormat{Separator: '\t', DateFormat: "2006-01-02T15:04:05Z07:00"},
+			},
+		},
+		{
+			args: []string{"RFC3339Nano"},
+			expected: options{
+				dateFormat: "2006-01-02T15:04:05.999999999Z07:00",
+				separator:  '\t',
+				lineFormat: format.LineFormat{Separator: '\t', DateFormat: "2006-01-02T15:04:05.999999999Z07:00"},
+			},
+		},
+		{
+			args: []string{"Kitchen"},
+			expected: options{
+				dateFormat: "3:04PM",
+				separator:  '\t',
+				lineFormat: format.LineFormat{Separator: '\t', DateFormat: "3:04PM"},
+			},
+		},
+		{
+			args: []string{"Stamp"},
+			expected: options{
+				dateFormat: "Jan _2 15:04:05",
+				separator:  '\t',
+				lineFormat: format.LineFormat{Separator: '\t', DateFormat: "Jan _2 15:04:05"},
+			},
+		},
+		{
+			args: []string{"StampMilli"},
+			expected: options{
+				dateFormat: "Jan _2 15:04:05.000",
+				separator:  '\t',
+				lineFormat: format.LineFormat{Separator: '\t', DateFormat: "Jan _2 15:04:05.000"},
+			},
+		},
+		{
+			args: []string{"StampMicro"},
+			expected: options{
+				dateFormat: "Jan _2 15:04:05.000000",
+				separator:  '\t',
+				lineFormat: format.LineFormat{Separator: '\t', DateFormat: "Jan _2 15:04:05.000000"},
+			},
+		},
+		{
+			args: []string{"mysql"},
+			expected: options{
+				dateFormat: "2006-01-02 15:04:05",
+				separator:  '\t',
+				lineFormat: format.LineFormat{Separator: '\t', DateFormat: "2006-01-02 15:04:05"},
+			},
+		},
+		{
+			args: []string{"debug"},
+			expected: options{
+				separator:  '\t',
+				lineFormat: format.LineFormat{Separator: '\t'},
+				debug:      true,
+			},
+		},
+		{
+			args:  []string{"help"},
+			fails: true,
+		},
 	}
 
 	for _, ts := range tests {
